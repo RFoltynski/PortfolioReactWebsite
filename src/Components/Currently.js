9ddf3c805479cebc1e5acb15bdb137dfc139a1d6
Rafal <foltynski.rafal@gmail.com>
2019-05-29 23:23:12 +0200

    first try with SVG animations

--- src/Components/Currently.js
+++ src/Components/Currently.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ReactSVG from "react-svg";
 import "../CSS/Currently.css";
 import bubble from "../Image/bubble.png";
 import bubbleSVG from "../Image/bubble.svg";
@@ -9,7 +10,31 @@ class Currently extends Component {
     return (
       <div className="currently-box">
         <div className="currently-box-speech">
-          <img src={bubbleSVG} className="currently-box-bubbleSVG" />{" "}
+          <ReactSVG
+            src={bubbleSVG}
+            afterInjection={(error, svg) => {
+              if (error) {
+                console.error(error);
+                return;
+              }
+              console.log(svg);
+            }}
+            beforeInjection={svg => {
+              svg.classList.add("currently-box-bubbleSVG");
+            }}
+            evalScripts="always"
+            fallback={() => <span>Error!</span>}
+            loading={() => <span>Loading</span>}
+            renumerateIRIElements={false}
+            wrapper="span"
+            className="currently-box-bubbleSVG"
+            onClick={() => {
+              console.log("wrapper onClick");
+            }}
+          />
+          <div className="currently-box-bubbleSVG-text">
+            Jestem Rafał. <br />I lubie Reacta!
+          </div>
           <img src={rafal} className="currently-box-img" />{" "}
         </div>
       </div>
