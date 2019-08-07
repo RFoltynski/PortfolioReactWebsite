02de72f8bff87722afbe508439c28983766b15de
RFoltynski <Foltynski.rafal@gmail.com>
2019-08-07 21:52:52 +0200

    questions animation added

--- src/Components/QA.js
+++ src/Components/QA.js
@@ -1,22 +1,178 @@
 import React, { Component } from "react";
+import { Spring, Transition, animated } from "react-spring/renderprops.cjs";
 import "../CSS/QA.css";
 
 class QA extends Component {
+  state = {
+    showQuestion1: false,
+    showQuestion2: false,
+    showQuestion3: false,
+    showQuestion4: false
+  };
+
   render() {
     return (
-      <div className="project3-box">
+      <div className="QA-box">
         <div className="header">
           <h2>Q&A</h2>
-          <p>
-            Apparently we had reached a great height in the atmosphere, for the
-            sky was a dead black, and the stars had ceased to twinkle. By the
-            same illusion which lifts the horizon of the sea to the level of the
-            spectator on a hillside, the sable cloud beneath was dished out, and
-            the car seemed to float in the middle of an immense dark sphere,
-            whose upper half was strewn with silver. Looking down into the dark
-            gulf below, I could see a ruddy light streaming through a rift in
-            the clouds.
-          </p>
+          <div className="QA-box-questions">
+            <div className="QA-box-question">
+              <div className="row">
+                <div className="QA-box-question-title">
+                  Pellentesque ac velit eget turpis dictum pulvinar vitae vel
+                  urna. Vestibulum at sagittis ante, vel facilisis tortor.
+                </div>
+                <button
+                  onClick={() =>
+                    this.setState({
+                      showQuestion1: !this.state.showQuestion1
+                    })
+                  }
+                >
+                  rozwin
+                </button>
+              </div>
+              <Transition
+                native
+                items={this.state.showQuestion1}
+                from={{ opacity: 0, marginTop: -100 }}
+                enter={{ opacity: 1, marginTop: 0 }}
+                leave={{ opacity: 0, marginTop: -100 }}
+              >
+                {show =>
+                  show &&
+                  (props => (
+                    <animated.div style={props}>
+                      <div className="QA-box-question-answer">
+                        Pellentesque ac velit eget turpis dictum pulvinar vitae
+                        vel urna. Vestibulum at sagittis ante, vel facilisis
+                        tortor.Pellentesque ac velit eget turpis dictum pulvinar
+                        vitae vel urna. Vestibulum at sagittis ante, vel
+                        facilisis tortor.
+                      </div>
+                    </animated.div>
+                  ))
+                }
+              </Transition>
+            </div>
+            <div className="QA-box-question">
+              <div className="row">
+                <div className="QA-box-question-title">
+                  Pellentesque ac velit eget turpis dictum pulvinar vitae vel
+                  urna. Vestibulum at sagittis ante, vel facilisis tortor.
+                </div>
+                <button
+                  onClick={() =>
+                    this.setState({
+                      showQuestion2: !this.state.showQuestion2
+                    })
+                  }
+                >
+                  rozwin
+                </button>
+              </div>
+              <Transition
+                native
+                items={this.state.showQuestion2}
+                from={{ opacity: 0, marginTop: -100 }}
+                enter={{ opacity: 1, marginTop: 0 }}
+                leave={{ opacity: 0, marginTop: -100 }}
+              >
+                {show =>
+                  show &&
+                  (props => (
+                    <animated.div style={props}>
+                      <div className="QA-box-question-answer">
+                        Pellentesque ac velit eget turpis dictum pulvinar vitae
+                        vel urna. Vestibulum at sagittis ante, vel facilisis
+                        tortor.Pellentesque ac velit eget turpis dictum pulvinar
+                        vitae vel urna. Vestibulum at sagittis ante, vel
+                        facilisis tortor.
+                      </div>
+                    </animated.div>
+                  ))
+                }
+              </Transition>
+            </div>
+            <div className="QA-box-question">
+              <div className="row">
+                <div className="QA-box-question-title">
+                  Pellentesque ac velit eget turpis dictum pulvinar vitae vel
+                  urna. Vestibulum at sagittis ante, vel facilisis tortor.
+                </div>
+                <button
+                  onClick={() =>
+                    this.setState({
+                      showQuestion3: !this.state.showQuestion3
+                    })
+                  }
+                >
+                  rozwin
+                </button>
+              </div>
+              <Transition
+                native
+                items={this.state.showQuestion3}
+                from={{ opacity: 0, marginTop: -100 }}
+                enter={{ opacity: 1, marginTop: 0 }}
+                leave={{ opacity: 0, marginTop: -100 }}
+              >
+                {show =>
+                  show &&
+                  (props => (
+                    <animated.div style={props}>
+                      <div className="QA-box-question-answer">
+                        Pellentesque ac velit eget turpis dictum pulvinar vitae
+                        vel urna. Vestibulum at sagittis ante, vel facilisis
+                        tortor.Pellentesque ac velit eget turpis dictum pulvinar
+                        vitae vel urna. Vestibulum at sagittis ante, vel
+                        facilisis tortor.
+                      </div>
+                    </animated.div>
+                  ))
+                }
+              </Transition>
+            </div>
+            <div className="QA-box-question">
+              <div className="row">
+                <div className="QA-box-question-title">
+                  Pellentesque ac velit eget turpis dictum pulvinar vitae vel
+                  urna. Vestibulum at sagittis ante, vel facilisis tortor.
+                </div>
+                <button
+                  onClick={() =>
+                    this.setState({
+                      showQuestion4: !this.state.showQuestion4
+                    })
+                  }
+                >
+                  rozwin
+                </button>
+              </div>
+              <Transition
+                native
+                items={this.state.showQuestion4}
+                from={{ opacity: 0, marginTop: -100 }}
+                enter={{ opacity: 1, marginTop: 0 }}
+                leave={{ opacity: 0, marginTop: -100 }}
+              >
+                {show =>
+                  show &&
+                  (props => (
+                    <animated.div style={props}>
+                      <div className="QA-box-question-answer">
+                        Pellentesque ac velit eget turpis dictum pulvinar vitae
+                        vel urna. Vestibulum at sagittis ante, vel facilisis
+                        tortor.Pellentesque ac velit eget turpis dictum pulvinar
+                        vitae vel urna. Vestibulum at sagittis ante, vel
+                        facilisis tortor.
+                      </div>
+                    </animated.div>
+                  ))
+                }
+              </Transition>
+            </div>
+          </div>
         </div>
       </div>
     );
